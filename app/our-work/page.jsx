488924a6497b3b872c3894ec58b18caf9500a4cd
alisaghir1 <alisaghir543@gmail.com>
2025-01-29 14:51:29 +0200

    finished the single project page

--- app/our-work/page.jsx
+++ app/our-work/page.jsx
@@ -32,109 +32,117 @@ const OurWork = () => {
         </div>
       </div>
       <div>
-      <section className="grid grid-cols-2 mt-20 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5 px-5 xl:mx-20 mb-20">
-        {/* Residental Tower */}
-        <Link href="/our-work/residentalTowerDubai" className="block">
-          <div className="relative hover:bg-[url(/projects/residentalTower.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tl-full">
-            <div className="text-white xl:p-3 rounded-lg w-full text-start">
-              <h2 className="xl:text-lg text-md mb-2 font-semibold">
-                Residental Tower
-              </h2>
-              <p className="xl:text-sm text-xs">Dubai, United Arab Emirates</p>
+        <section className="grid grid-cols-2 mt-20 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5 px-5 xl:mx-20 mb-20">
+          {/* Residental Tower */}
+          <Link href="/our-work/residentalTowerDubai" className="block">
+            <div className="relative hover:bg-[url(/projects/residentalTower.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tl-full">
+              <div className="text-white xl:p-3 rounded-lg w-full text-start">
+                <h2 className="xl:text-lg text-md mb-2 font-semibold">
+                  Residental Tower
+                </h2>
+                <p className="xl:text-sm text-xs">
+                  Dubai, United Arab Emirates
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
 
-        {/* Villa */}
-        <Link href="/our-work/villaDubai" className="block">
-          <div className="relative hover:bg-[url(/projects/villa.jpg)] bg-black flex transition-all duration-300 ease-in-out pt-4 items-start justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-bl-full">
-            <div className="text-white xl:p-3 rounded-lg w-full text-start">
-              <h2 className="xl:text-lg text-md mb-2 font-semibold">Villa</h2>
-              <p className="xl:text-sm text-xs">
-                Dubai Hills, United Arab Emirates
-              </p>
+          {/* Villa */}
+          <Link href="/our-work/villaDubai" className="block">
+            <div className="relative hover:bg-[url(/projects/villa.jpg)] bg-black flex transition-all duration-300 ease-in-out pt-4 items-start justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-bl-full">
+              <div className="text-white xl:p-3 rounded-lg w-full text-start">
+                <h2 className="xl:text-lg text-md mb-2 font-semibold">Villa</h2>
+                <p className="xl:text-sm text-xs">
+                  Dubai Hills, United Arab Emirates
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
 
-        {/* Residental Tower - Sharjah */}
-        <Link href="/our-work/residentalTowerSharjah" className="block">
-          <div className="relative hover:bg-[url(/projects/residentalTower.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tr-full">
-            <div className="text-white xl:p-3 rounded-lg w-full text-start">
-              <h2 className="xl:text-lg text-md mb-2 font-semibold">
-                Residental Tower
-              </h2>
-              <p className="xl:text-sm text-xs">
-                Sharjah, United Arab Emirates
-              </p>
+          {/* Residental Tower - Sharjah */}
+          <Link href="/our-work/residentalTowerSharjah" className="block">
+            <div className="relative hover:bg-[url(/projects/residentalTower.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tr-full">
+              <div className="text-white xl:p-3 rounded-lg w-full text-start">
+                <h2 className="xl:text-lg text-md mb-2 font-semibold">
+                  Residental Tower
+                </h2>
+                <p className="xl:text-sm text-xs">
+                  Sharjah, United Arab Emirates
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
 
-        {/* Commercial Building */}
-        <Link href="/our-work/commercialBuilding" className="block">
-          <div className="relative hover:bg-[url(/projects/commercialBuilding.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tr-full">
-            <div className="text-white xl:p-3 rounded-lg w-full text-start">
-              <h2 className="xl:text-lg text-md mb-2 font-semibold">
-                Commercial Building
-              </h2>
-              <p className="xl:text-sm text-xs">
-                Sharjah, United Arab Emirates
-              </p>
+          {/* Commercial Building */}
+          <Link href="/our-work/commercialBuilding" className="block">
+            <div className="relative hover:bg-[url(/projects/commercialBuilding.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tr-full">
+              <div className="text-white xl:p-3 rounded-lg w-full text-start">
+                <h2 className="xl:text-lg text-md mb-2 font-semibold">
+                  Commercial Building
+                </h2>
+                <p className="xl:text-sm text-xs">
+                  Sharjah, United Arab Emirates
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
 
-        {/* Residental Building */}
-        <Link href="/our-work/residentalBuildingSharjah" className="block">
-          <div className="relative hover:bg-[url(/projects/residentalBuilding.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tl-full">
-            <div className="text-white xl:p-3 rounded-lg w-full text-start">
-              <h2 className="xl:text-lg text-md mb-2 font-semibold">
-                Residental Building
-              </h2>
-              <p className="xl:text-sm text-xs">
-                Sharjah, United Arab Emirates
-              </p>
+          {/* Residental Building */}
+          <Link href="/our-work/residentalBuildingSharjah" className="block">
+            <div className="relative hover:bg-[url(/projects/residentalBuilding.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tl-full">
+              <div className="text-white xl:p-3 rounded-lg w-full text-start">
+                <h2 className="xl:text-lg text-md mb-2 font-semibold">
+                  Residental Building
+                </h2>
+                <p className="xl:text-sm text-xs">
+                  Sharjah, United Arab Emirates
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
 
-        {/* Warehouse */}
-        <Link href="/our-work/warehouseAlQouz" className="block">
-          <div className="relative hover:bg-[url(/projects/warehouse.jpg)] bg-black flex transition-all duration-300 ease-in-out pt-4 items-start justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-br-full">
-            <div className="text-white xl:p-3 rounded-lg w-full text-start">
-              <h2 className="xl:text-lg text-md mb-2 font-semibold">
-                Warehouse
-              </h2>
-              <p className="xl:text-sm text-xs">Al Qouz, United Arab Emirates</p>
+          {/* Warehouse */}
+          <Link href="/our-work/warehouseAlQouz" className="block">
+            <div className="relative hover:bg-[url(/projects/warehouse.jpg)] bg-black flex transition-all duration-300 ease-in-out pt-4 items-start justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-br-full">
+              <div className="text-white xl:p-3 rounded-lg w-full text-start">
+                <h2 className="xl:text-lg text-md mb-2 font-semibold">
+                  Warehouse
+                </h2>
+                <p className="xl:text-sm text-xs">
+                  Al Qouz, United Arab Emirates
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
 
-        {/* Residental Building 2 */}
-        <Link href="/our-work/residentalBuildingSharjah2" className="block">
-          <div className="relative hover:bg-[url(/projects/residentalBuilding2.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tl-full">
-            <div className="text-white xl:p-3 rounded-lg w-full text-start">
-              <h2 className="xl:text-lg text-md mb-2 font-semibold">
-                Residental Building
-              </h2>
-              <p className="xl:text-sm text-xs">
-                Sharjah, United Arab Emirates
-              </p>
+          {/* Residental Building 2 */}
+          <Link href="/our-work/residentalBuildingSharjah2" className="block">
+            <div className="relative hover:bg-[url(/projects/residentalBuilding2.jpg)] bg-black flex transition-all duration-300 ease-in-out pb-4 items-end justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-tl-full">
+              <div className="text-white xl:p-3 rounded-lg w-full text-start">
+                <h2 className="xl:text-lg text-md mb-2 font-semibold">
+                  Residental Building
+                </h2>
+                <p className="xl:text-sm text-xs">
+                  Sharjah, United Arab Emirates
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
 
-        {/* Factory */}
-        <Link href="/our-work/factoryAlQouz" className="block">
-          <div className="relative hover:bg-[url(/projects/factory.jpg)] bg-black flex transition-all duration-300 ease-in-out pt-4 items-start justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-br-full">
-            <div className="text-white xl:p-3 rounded-lg w-full text-start">
-              <h2 className="xl:text-lg text-md mb-2 font-semibold">Factory</h2>
-              <p className="xl:text-sm text-xs">Al Qouz, United Arab Emirates</p>
+          {/* Factory */}
+          <Link href="/our-work/factoryAlQouz" className="block">
+            <div className="relative hover:bg-[url(/projects/factory.jpg)] bg-black flex transition-all duration-300 ease-in-out pt-4 items-start justify-start h-[25rem] xl:h-[36rem] bg-cover bg-center shadow-lg text-white px-4 rounded-br-full">
+              <div className="text-white xl:p-3 rounded-lg w-full text-start">
+                <h2 className="xl:text-lg text-md mb-2 font-semibold">
+                  Factory
+                </h2>
+                <p className="xl:text-sm text-xs">
+                  Al Qouz, United Arab Emirates
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
-      </section>
+          </Link>
+        </section>
       </div>
       <VidioComponent />
     </div>
