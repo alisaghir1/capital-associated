4d1e0724b090f1dc8c742c8b5dc34f8b209b45b5
alisaghir1 <alisaghir543@gmail.com>
2025-02-03 13:45:17 +0200

    added a transition in the footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -10,12 +10,12 @@ const Footer = () => {
           <div>
             <h3 className="text-lg font-bold mb-4 border-b-2 border-b-black pb-4 w-fit">Quick Links</h3>
             <ul className="space-y-2">
-              <li><Link href="/" className="hover:text-gray-300">Home</Link></li>
-              <li><Link href="/about-us" className="hover:text-gray-300">About us</Link></li>
-              <li><Link href="/services" className="hover:text-gray-300">Our Services</Link></li>
-              <li><Link href="/our-work" className="hover:text-gray-300">Our Work</Link></li>
-              <li><Link href="/blog" className="hover:text-gray-300">Blog</Link></li>
-              <li><Link href="/contact-us" className="hover:text-gray-300">Contact Us</Link></li>
+              <li><Link href="/" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Home</Link></li>
+              <li><Link href="/about-us" className="hover:text-gray-300 transition-all duration-200 ease-in-out">About us</Link></li>
+              <li><Link href="/services" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Our Services</Link></li>
+              <li><Link href="/our-work" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Our Work</Link></li>
+              <li><Link href="/blog" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Blog</Link></li>
+              <li><Link href="/contact-us" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Contact Us</Link></li>
             </ul>
           </div>
 
@@ -23,15 +23,15 @@ const Footer = () => {
           <div>
             <h3 className="text-lg font-bold mb-4 border-b-2 border-b-black pb-4 w-fit">Services</h3>
             <ul className="space-y-2">
-              <li><Link href="/services/general-contracting" className="hover:text-gray-300">General Contracting</Link></li>
-              <li><Link href="/services/construction-management" className="hover:text-gray-300">Construction Management</Link></li>
-              <li><Link href="/services/design-build" className="hover:text-gray-300">Design - Build</Link></li>
-              <li><Link href="/services/renovation-and-remodeling" className="hover:text-gray-300">Renovation & Remodeling</Link></li>
-              <li><Link href="/services/pre-construction-services" className="hover:text-gray-300">Pre-Construction Services</Link></li>
-              <li><Link href="/services/value-engineering" className="hover:text-gray-300">Value Engineering</Link></li>
-              <li><Link href="/services/green-building-solutions" className="hover:text-gray-300">Green Building Solutions</Link></li>
-              <li><Link href="/services/specialty-construction" className="hover:text-gray-300">Specialty Construction</Link></li>
-              <li><Link href="/services/interior-fit-out" className="hover:text-gray-300">Interior Fit-Out</Link></li>
+              <li><Link href="/services/general-contracting" className="hover:text-gray-300 transition-all duration-200 ease-in-out">General Contracting</Link></li>
+              <li><Link href="/services/construction-management" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Construction Management</Link></li>
+              <li><Link href="/services/design-build" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Design - Build</Link></li>
+              <li><Link href="/services/renovation-and-remodeling" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Renovation & Remodeling</Link></li>
+              <li><Link href="/services/pre-construction-services" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Pre-Construction Services</Link></li>
+              <li><Link href="/services/value-engineering" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Value Engineering</Link></li>
+              <li><Link href="/services/green-building-solutions" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Green Building Solutions</Link></li>
+              <li><Link href="/services/specialty-construction" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Specialty Construction</Link></li>
+              <li><Link href="/services/interior-fit-out" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Interior Fit-Out</Link></li>
             </ul>
           </div>
 
@@ -41,8 +41,8 @@ const Footer = () => {
             <address className="space-y-2">
               <p>Dubai. JLT. Cluster Y</p>
               <p>Office 1501</p>
-              <p>Mobile: <Link href="tel:+971521211520" className="hover:text-gray-300">+971 52 121 1520</Link></p>
-              <p>Email: <Link href="mailto:hello@capitalassociated.com" className="hover:text-gray-300">hello@capitalassociated.com</Link></p>
+              <p>Mobile: <Link href="tel:+971521211520" className="hover:text-gray-300 transition-all duration-200 ease-in-out">+971 52 121 1520</Link></p>
+              <p>Email: <Link href="mailto:hello@capitalassociated.com" className="hover:text-gray-300 transition-all duration-200 ease-in-out">hello@capitalassociated.com</Link></p>
             </address>
           </div>
 
@@ -50,10 +50,10 @@ const Footer = () => {
           <div>
             <h3 className="text-lg font-bold mb-4 border-b-2 border-b-black pb-4 w-fit">Follow Us</h3>
             <ul className="space-y-2">
-              <li><Link href="https://www.facebook.com" className="hover:text-gray-300">Facebook</Link></li>
-              <li><Link href="https://www.instagram.com/capital.associated/" className="hover:text-gray-300">Instagram</Link></li>
-              <li><Link href="https://www.youtube.com" className="hover:text-gray-300">YouTube</Link></li>
-              <li><Link href="https://www.twitter.com" className="hover:text-gray-300">X</Link></li>
+              <li><Link href="https://www.facebook.com" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Facebook</Link></li>
+              <li><Link href="https://www.instagram.com/capital.associated/" className="hover:text-gray-300 transition-all duration-200 ease-in-out">Instagram</Link></li>
+              <li><Link href="https://www.youtube.com" className="hover:text-gray-300 transition-all duration-200 ease-in-out">YouTube</Link></li>
+              <li><Link href="https://www.twitter.com" className="hover:text-gray-300 transition-all duration-200 ease-in-out">X</Link></li>
             </ul>
           </div>
         </div>
